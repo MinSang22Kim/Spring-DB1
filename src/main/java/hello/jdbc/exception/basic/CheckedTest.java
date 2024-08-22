301852bfa9948c7a636b7a38fd9b53f4738db197
MinSang22Kim <powerminsang9901@gmail.com>
2024-08-22 20:10:37 +0900

    섹션5. 자바 예외 이해 - 체크 예외 기본 이해

--- src/main/java/hello/jdbc/exception/basic/CheckedTest.java
+++ src/main/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -1,9 +1,9 @@
 package hello.jdbc.exception.basic;
 
 import lombok.extern.slf4j.Slf4j;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 public class CheckedTest {
